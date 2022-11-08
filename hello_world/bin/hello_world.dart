a97ebc36d1ba8991086303fe30499448bae8cf01
alex-pythonista <indrajit.alex@gmail.com>
2022-11-08 22:24:28 +0600

    learned variables so far

--- hello_world/bin/hello_world.dart
+++ hello_world/bin/hello_world.dart
@@ -1,3 +1,8 @@
 void main() {
-  print('Hello world');
+  String name = "Alex";
+  int age = 23;
+  print("There once was a man named $name.");
+  print("He was $age");
+  print("He really liked the name $name.");
+  print("But he didn't like being $age years old");
 }
