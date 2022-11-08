9250e1fcb79506085669955f0ac0a6c6df20af58
alex-pythonista <indrajit.alex@gmail.com>
2022-11-09 02:41:06 +0600

    madibs game

--- hello_world/bin/hello_world.dart
+++ hello_world/bin/hello_world.dart
@@ -3,11 +3,16 @@ import 'dart:io';
 
 void main() {
   // int x, y;
-  print("X = ?");
-  int x = int.parse(stdin.readLineSync()!);
+  print("Enter your fav color: ");
+  String color = stdin.readLineSync()!;
 
-  print("Y = ?");
-  int y = int.parse(stdin.readLineSync()!);
+  print("Enter a plural noun: ");
+  String noun = stdin.readLineSync()!;
 
-  print("The sum of $x and $y is ${x + y}");
+  print("Enter your fav celeb name: ");
+  String celeb = stdin.readLineSync()!;
+
+  print("Roses are $color");
+  print("$noun are blue");
+  print("I love $celeb!");
 }
