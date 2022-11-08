86941051677535c7aa20aecaeec8bd8bc370dac5
alex-pythonista <indrajit.alex@gmail.com>
2022-11-09 03:02:23 +0600

    List data structure

--- hello_world/bin/hello_world.dart
+++ hello_world/bin/hello_world.dart
@@ -3,16 +3,6 @@ import 'dart:io';
 
 void main() {
   // int x, y;
-  print("Enter your fav color: ");
-  String color = stdin.readLineSync()!;
-
-  print("Enter a plural noun: ");
-  String noun = stdin.readLineSync()!;
-
-  print("Enter your fav celeb name: ");
-  String celeb = stdin.readLineSync()!;
-
-  print("Roses are $color");
-  print("$noun are blue");
-  print("I love $celeb!");
+  List<int> oddNum = [1, 3, 5, 7, 9];
+  print(oddNum.sublist(3, 4));
 }
