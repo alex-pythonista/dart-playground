ed3deadb09cddefc433b2cc60b87fee0843a0c0c
alex-pythonista <indrajit.alex@gmail.com>
2022-11-09 02:10:38 +0600

    learned about the basic data types

--- hello_world/bin/hello_world.dart
+++ hello_world/bin/hello_world.dart
@@ -1,8 +1,6 @@
+import 'dart:math';
+
 void main() {
-  String name = "Alex";
-  int age = 23;
-  print("There once was a man named $name.");
-  print("He was $age");
-  print("He really liked the name $name.");
-  print("But he didn't like being $age years old");
+  int x = 3, y = 5;
+  print(sqrt(x));
 }
