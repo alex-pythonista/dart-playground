0c4fc9ed9716f9d6795e732cd26149258b229ca9
alex-pythonista <indrajit.alex@gmail.com>
2022-11-10 02:39:39 +0600

    for loops

--- hello_world/bin/hello_world.dart
+++ hello_world/bin/hello_world.dart
@@ -8,22 +8,9 @@ String prompt(String promptText) {
 }
 
 void main() {
-  print("Enter the opcode (+. -, *, /): ");
-  String opcode = stdin.readLineSync()!;
-  print("Enter operand1: ");
-  int num1 = int.parse(stdin.readLineSync()!);
-  print("Enter operand2: ");
-  int num2 = int.parse(stdin.readLineSync()!);
+  List<String> friends = ["Arman", "Delu", "Shehab", "Rabby"];
 
-  if (opcode == "+") {
-    print("The sum is ${num1 + num2}");
-  } else if (opcode == "-") {
-    print("The subtraction is ${num1 - num2}");
-  } else if (opcode == "*") {
-    print("The multiplication is ${num1 * num2}");
-  } else if (opcode == "/") {
-    print("The division is ${num1 / num2}");
-  } else {
-    print("The opcode $opcode is not known! Please enter a valid opcode.");
+  for (int i = 0; i < friends.length; i++) {
+    print(friends[i]);
   }
 }
