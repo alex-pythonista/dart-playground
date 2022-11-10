e27433336302a5a350cf0ab0540058b03cdac94f
alex-pythonista <indrajit.alex@gmail.com>
2022-11-10 22:00:30 +0600

    Math Quiz

--- hello_world/bin/hello_world.dart
+++ hello_world/bin/hello_world.dart
@@ -1,26 +1,42 @@
-// ignore_for_file: unnecessary_this
-
 import 'dart:math';
 import 'dart:io';
 
-class Student {
-  String? name;
-  double? gpa;
+double doublePrompt(String? text) {
+  print(text);
+  double n = double.parse(stdin.readLineSync()!);
+  return n;
+}
 
-  Student(String aName, double aGpa) {
-    this.name = aName;
-    this.gpa = aGpa;
-  }
+class MathQuestion {
+  String? question;
+  double? answer;
 
-  bool hasHonors() {
-    return this.gpa! >= 3.5;
+  MathQuestion(String aQuestion, double aAnswer) {
+    this.question = aQuestion;
+    this.answer = aAnswer;
   }
 }
 
 void main() {
-  Student jim = Student("Jim", 2.5);
-  Student pim = Student("Pim", 3.5);
+  // MathQuestion q1 = MathQuestions("3 + 5", 5);
+  // MathQuestion q2 = MathQuestions("7.5 + 2. 5", 10);
+  // MathQuestion q3 = MathQuestions("87 + 3.65", 90.65);
 
-  print(jim.hasHonors());
-  print(pim.hasHonors());
+  List<MathQuestion> questions = [
+    MathQuestion("3 + 5 = ?", 8),
+    MathQuestion("7.5 + 2. 5 = ?", 10),
+    MathQuestion("87 + 3.65 = ?", 90.65)
+  ];
+  int score = 0;
+  for (MathQuestion mq in questions) {
+    double ans = doublePrompt(mq.question);
+    if (ans == mq.answer) {
+      score += 1;
+      print("Great!!!");
+    } else {
+      print("Wrong!! The correct answer is ${mq.answer}");
+    }
+  }
+
+  print("Your scored $score");
 }
