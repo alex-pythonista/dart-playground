463487ddaa6137da5daa4b3fda4a163bffab9986
alex-pythonista <indrajit.alex@gmail.com>
2022-11-13 03:02:07 +0600

    method overriding

--- hello_world/bin/hello_world.dart
+++ hello_world/bin/hello_world.dart
@@ -1,49 +1,18 @@
-import 'dart:math';
-import 'dart:io';
-
-double doublePrompt(String? text) {
-  print(text);
-  double n = double.parse(stdin.readLineSync()!);
-  return n;
-}
-
-class MathQuestion {
-  String? question;
-  double? answer;
-
-  MathQuestion(String aQuestion, double aAnswer) {
-    this.question = aQuestion;
-    this.answer = aAnswer;
-  }
-  bool checkAnswer(double ans) {
-    if (ans == this.answer) {
-      print("Great!!!");
-      return true;
-    } else {
-      print("Wrong!! The correct answer is ${this.answer}");
-      return false;
-    }
-  }
-}
-
 void main() {
-  // MathQuestion q1 = MathQuestions("3 + 5", 5);
-  // MathQuestion q2 = MathQuestions("7.5 + 2. 5", 10);
-  // MathQuestion q3 = MathQuestions("87 + 3.65", 90.65);
+  // String x;
+  Child c = new Child();
+  c.m1(12);
+}
 
-  List<MathQuestion> questions = [
-    MathQuestion("3 + 5 = ?", 8),
-    MathQuestion("7.5 + 2. 5 = ?", 10),
-    MathQuestion("87 + 3.65 = ?", 90.65)
-  ];
-
-  int score = 0;
-  for (MathQuestion mq in questions) {
-    double ans = doublePrompt(mq.question);
-    if (mq.checkAnswer(ans) == true) {
-      score++;
-    }
+class Parent {
+  void m1(int a) {
+    print("value of a $a");
   }
+}
 
-  print("Your scored $score");
+class Child extends Parent {
+  @override
+  void m1(int b) {
+    print("value of b $b");
+  }
 }
